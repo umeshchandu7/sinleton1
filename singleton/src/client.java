02bc2a47f4adf1adfd205e2761f45359ac59abbc
umesh <umeshdmc7@gmail.com>
2023-06-29 08:00:08 +0530

    singleton

--- singleton/src/client.java
+++ singleton/src/client.java
@@ -1,2 +1,12 @@
 public class client {
+    public static void main(String[] args) {
+        singleton s1 = singleton.getInstance();
+        singleton s2 = singleton.getInstance();
+        singleton s3 = singleton.getInstance();
+        s1.getConnect();
+        s2.getConnect();
+        s1.getConnect();
+        s3.getConnect();
+
+    }
 }
--- singleton/src/singleton.java
+++ singleton/src/singleton.java
@@ -1,2 +1,33 @@
-public class singleton {
+public class singleton  {
+    private static singleton instance;
+    private int counter;
+    private singleton()
+    {
+      counter = 0;
+    }
+    public static singleton getInstance()
+        {
+            if(instance==null)
+            {
+                instance = new singleton();
+                return instance;
+            }
+            else
+            {
+                return instance;
+            }
+        }
+        public void getConnect()
+        {
+            if(counter<3)
+            {
+                counter++;
+                System.out.println(true);
+            }
+            else
+            {
+                System.out.println(false);
+            }
+
+        }
 }
